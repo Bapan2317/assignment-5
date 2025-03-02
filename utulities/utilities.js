0bb96fc6f154976d01693f6095ec5ce8985fc334
Bapan2317 <bapanm2317@gmail.com>
2025-03-02 20:47:48 +0530

    allmost done

--- utulities/utilities.js
+++ utulities/utilities.js
@@ -3,11 +3,37 @@ function getElementByIDtoInnerText(id) {
   return valueCount;
 }
 
-function getComment(){
-    const commentText = 'You have Complete The Task Add Dark Mode at 12:48:15 PM'
-    const commentContainer = document.getElementById('comment-container')
-    const commentElement = document.createElement('p')
-    commentElement.innerText = commentText;
-    commentContainer.appendChild(commentElement)
+function getComment() {
+  const commentText = "complete the task " + getTime();
+  const commentContainer = document.getElementById("comment-container");
+  const commentElement = document.createElement("p");
+  commentElement.innerText = commentText;
+  commentContainer.appendChild(commentElement);
 }
 
+//date
+
+function date() {
+  let currentDate = new Date();
+  let formattedDate = currentDate.toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return formattedDate;
+}
+
+// get time
+
+function getTime() {
+  const options = {
+    timeZone: "Asia/Kolkata",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  };
+  let currentTime = new Date().toLocaleTimeString("en-US", options);
+  return currentTime;
+}
